4104ad258bae848fe5a3fa2f16ed5bb0471afa7a
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-05-18 17:00:54 +0100

    Add method to read room file and create test file

--- YetAnotherDungeonCrawler/Controller.cs
+++ YetAnotherDungeonCrawler/Controller.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +12,29 @@ namespace YetAnotherDungeonCrawler
     /// </summary>
     public class Controller
     {
+        List<Room> rooms = new List<Room>();
+
         /// <summary>
         /// Method to start the game. The game loop will be here.
         /// </summary>
-        public void Start(){
+        public void Start()
+        {
+            ReadFile();
+        }
 
+        private void ReadFile()
+        {
+            string s;
+            using StreamReader r =
+                new StreamReader("./YetAnotherDungeonCrawler/Rooms.txt");
+
+            while ((s = r.ReadLine()) != null)
+            {
+                string[] roomSpecs = s.Split(',');
+                foreach (string i in roomSpecs){
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
