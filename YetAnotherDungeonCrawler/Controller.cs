1c4d7af9ceb86e0e0701048fb09e0fa8d36b3379
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-05-18 16:14:40 +0100

    Create Controller class

---
+++ YetAnotherDungeonCrawler/Controller.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetAnotherDungeonCrawler
+{
+    /// <summary>
+    /// Controller class that will handle all the game logic and communicate
+    /// with the View
+    /// </summary>
+    public class Controller
+    {
+        /// <summary>
+        /// Method to start the game. The game loop will be here.
+        /// </summary>
+        public void Start(){
+
+        }
+    }
+}
