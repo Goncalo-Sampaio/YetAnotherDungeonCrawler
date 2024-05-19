b2b6035969ff350151faf568ccff504f6e245d03
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-05-19 11:42:16 +0100

    Add method to read enemy file

--- YetAnotherDungeonCrawler/Controller.cs
+++ YetAnotherDungeonCrawler/Controller.cs
@@ -13,16 +13,17 @@ namespace YetAnotherDungeonCrawler
     public class Controller
     {
         List<Room> rooms = new List<Room>();
+        HashSet<Enemy> enemies = new HashSet<Enemy>();
 
         /// <summary>
         /// Method to start the game. The game loop will be here.
         /// </summary>
         public void Start()
         {
-            ReadFile();
+            ReadRoomFile();
         }
 
-        private void ReadFile()
+        private void ReadRoomFile()
         {
             string s;
             using StreamReader r =
@@ -35,22 +36,44 @@ namespace YetAnotherDungeonCrawler
                 string enemyName = roomSpecs[1];
                 int enemyHealth = int.Parse(roomSpecs[2]);
                 int enemyDamage = int.Parse(roomSpecs[3]);
+                //foreach (Enemy enemy in enemies){}
                 Enemy enemy = new Enemy(enemyName, enemyHealth, enemyDamage);
                 string item = roomSpecs[4];
                 int north = RoomExit(roomSpecs[5]);
                 int south = RoomExit(roomSpecs[6]);
                 int west = RoomExit(roomSpecs[7]);
                 int east = RoomExit(roomSpecs[8]);
-                int[] exits = new int[4]{north, south, west, east};
+                int[] exits = new int[4] { north, south, west, east };
                 //rooms.Add(new Room(id,enemy,item,exits));
             }
         }
 
-        private int RoomExit(string roomId){
-            if (roomId == "null"){
+        private void ReadEnemyFile()
+        {
+            string s;
+            using StreamReader r =
+                new StreamReader("./YetAnotherDungeonCrawler/Rooms.txt");
+
+            while ((s = r.ReadLine()) != null)
+            {
+                string[] enemySpecs = s.Split(',');
+                string enemyName = enemySpecs[0];
+                int enemyHealth = int.Parse(enemySpecs[1]);
+                int enemyAttack = int.Parse(enemySpecs[2]);
+
+                Enemy enemy = new Enemy(enemyName, enemyHealth, enemyAttack);
+                enemies.Add(enemy);
+            }
+        }
+
+        private int RoomExit(string roomId)
+        {
+            if (roomId == "null")
+            {
                 return 0;
             }
-            else{
+            else
+            {
                 return int.Parse(roomId);
             }
         }
